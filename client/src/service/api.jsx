00858036a89625cbd7e239c4fec9c92a4a413dab
Priyanshu Naik <priyanshunaik@Priyanshus-MacBook-Air.local>
2025-07-05 12:03:25 +0530

    Changes Updated

--- client/src/service/api.jsx
+++ client/src/service/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URL } from '../constraints/config';
 import { getAccessToken } from '../utils/common_utils';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://blogging-platform-dl3w.onrender.com/';
 
 const axiosInstances = axios.create({
     baseURL: API_URL,
